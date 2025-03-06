648f15bb6876aff91f0ac6f417756fda7fc457c9
jowaaans <joannemargareth.joaquin.cics@ust.edu.ph>
2025-03-06 17:20:41 +0800

    nasira xampp tae

--- hehe.php
+++ hehe.php
@@ -299,7 +299,7 @@ function formatDateTime($datetime) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="headers.css">
-    <link rel="stylesheet" href="Profile.css">
+    <link rel="stylesheet" href="Profiles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -422,8 +422,8 @@ function formatDateTime($datetime) {
                     <div class="pfp">
                         <img src="Profile-Pics/profile_icon.png" alt="" class="profile-icon">
                         <h6 class="cusID">CUSTOMER ID</h6>
-                        <h6 class="cusNum">NO. <?php echo $fetch_cust_info['customer_id'] ?></h6>
-                        <h6 class="cusMem"><?php echo $fetch_cust_info['customer_membership_status'] ?></h6>
+                        <h6 class="cusNum">NO. <?php echo $fetch_cust_info['c_id'] ?></h6>
+                        <h6 class="cusMem"><?php echo $fetch_cust_info['c_membership_status'] ?></h6>
                     </div>
 
 
