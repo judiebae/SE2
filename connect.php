ce2ddc1f0624d8ba070fd4a4adfa01373f8f09e8
judiebae <138972613+judiebae@users.noreply.github.com>
2025-03-10 18:39:30 +0800

    joanne updated

--- connect.php
+++ connect.php
@@ -4,10 +4,11 @@ $dbname = "fur_a_paw_intments";
 $username = "root";
 $password = "";
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
 }
+
 ?>
